396e429c6eddc4bb849a09dbc6f9e5be13ac206d
rushnightfir3 <ashyoda@gmail.com>
2020-01-12 13:42:23 -0500

    added icons and made signin modal go away

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const Header =props=>{
     return(
-        <header style={styles.container}>
+        <header className="header" style={styles.container}>
             <div style={styles.logoContainer}>
                 <img style={styles.logo}width="300px" height="100px" src={require('../../images/logo/PNG/CCG.png')} alt="logo"/>
                 <h1> Admin</h1>
@@ -21,10 +21,9 @@ export default Header;
 const styles={
     container:{
         display:'flex',
-        backgroundColor:'green',
+        backgroundColor:'#00E87F',
         color:'white',
         fontSize:'1.5em',
-        textTransform:'uppercase',
         width:'100%',
         justifyContent:'space-between',
         alignItems:'center',
@@ -35,6 +34,7 @@ const styles={
         display:'flex',
         justifyContent:'flex-start',
         color:'black',
+        marginLeft:'2rem',
         
     },
     logo:{
--- src/components/nav/NavigationLeft.js
+++ src/components/nav/NavigationLeft.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './NavigationLeft.css'
 const NavigationLeft=props=>{
     return(
         <div style={styles.container} className="nav-container">
-            <ul>
-                <li><a href="google.com">Cards</a></li>
-                <li><a href="google.com">Cards</a></li>
-                <li><a href="google.com">Cards</a></li>
+            <ul style={styles.list}>
+                <li style={styles.listItem}><img src={require("../../images/home.png")} alt="home icon"/> <a href="google.com">Homepage</a></li>
+                <li style={styles.listItem}> <img src={require("../../images/shelf.png")} alt="inventory icon" /><a href="google.com">Inventory</a></li>
+                <li style={styles.listItem}><img src={require("../../images/analytics.png")} alt="analtyics icon" /><a href="google.com">Analytics</a></li>
             </ul>
 
         </div>
@@ -18,7 +18,14 @@ export default NavigationLeft;
 const styles={
     container:{
         width:'12%',
-        backgroundColor:'black',
+        backgroundColor:'#141414',
         color:'white',
+    },
+    list:{
+        listStyle:'none',
+    },
+    listItem:{
+        textDecoration:'none',
+        underline:'none'
     }
 }
